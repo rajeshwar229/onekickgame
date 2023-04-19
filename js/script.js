42c93533d8e0c42ec3df8be60255e21f7682a2cc
Rajeshwar <rajeshwar229@gmail.com>
2023-04-19 22:50:23 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -308,7 +308,7 @@ $(function(){
                     screen.orientation.lock('landscape');
                     gameCtrl.attrChange($(this),'data-fullscreen','on');
                     gameCtrl.addContent($(this),'Exit Full Screen');
-                    screen.orientation.type.indexOf('landscape') !== -1 && DOM.gameArena.css({"bottom" : window.innerHeight/13.5});
+                    screen.orientation.type.indexOf('landscape') !== -1 && DOM.gameArena.css({"bottom" : window.innerHeight/12.5});
                 }
                 else {
                     screen.orientation.unlock();
